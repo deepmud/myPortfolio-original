b0980fca520209a5dfb7bcb25e81289294924207
deepmud <73426734+deepmud@users.noreply.github.com>
2021-03-04 05:05:21 +0100

    Initial commit

---
+++ src/main/java/anikputa/daniel/chim/myPortfolio/MyRestController.java
@@ -0,0 +1,40 @@
+package anikputa.daniel.chim.myPortfolio;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController()
+public class MyRestController {
+    @GetMapping("/")
+    public ModelAndView index_page(){
+        ModelAndView mvc;
+        mvc = new ModelAndView();
+        mvc.setViewName("index");
+        return mvc;
+    }
+
+    @GetMapping("/education")
+    public ModelAndView education_page(){
+        ModelAndView mvc;
+        mvc = new ModelAndView();
+        mvc.setViewName("education");
+        return mvc;
+    }
+    @GetMapping("/skills")
+    public ModelAndView skills_page(){
+        ModelAndView mvc;
+        mvc = new ModelAndView();
+        mvc.setViewName("skills");
+        return mvc;
+    }
+
+    @GetMapping("/projects")
+    public ModelAndView project_page(){
+        ModelAndView mvc;
+        mvc = new ModelAndView();
+        mvc.setViewName("projects");
+        return mvc;
+    }
+}
---
+++ src/test/java/anikputa/daniel/chim/myPortfolio/MyPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package anikputa.daniel.chim.myPortfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
